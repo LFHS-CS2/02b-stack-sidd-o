3b24468f4eb1e22c9f38ccb1c4a1a5f263427a2d
sidd-o <69946996+sidd-o@users.noreply.github.com>
2020-09-24 22:23:38 -0500

    Update ArrayStackTest.java

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -16,20 +16,10 @@ public class ArrayStackTest {
 	}
 
 	/**
-	 * This is an example test. After you have written your own tests, get rid of
-	 * it.
-	 */
-	@Test
-	public void test_example() {
-		assertTrue("it should be true", 5 == 5);
-		assertEquals("they should be equal", 5, 5);
-		// when comparing doubles, the last number is a threshold
-		// of how close the numbers should be to be considered equal
-		assertEquals("they should be with .01 of each other", 5.12, 5.11, .01);
-		if (false)
-			fail("It should have been true");
-	}
-
+	*
+	* Test Various Cases Using the ArrayStack Class
+	*
+	*/
 	@Test
 	public void test_stack() {
 		ArrayStack temp = new ArrayStack();
