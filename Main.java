e938ee3c75560a1e0f5f1eb8235cd61fea3d7492
sidd-o <69946996+sidd-o@users.noreply.github.com>
2020-09-24 22:14:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import java.util.*;
 class Main {
 	public static void main(String[] args) {
 		System.out.println("-Running Tests on ArrayStack Class...");
-		//ArrayStackTest.main(null);
+		ArrayStackTest.main(null);
 		System.out.println("-ArrayStack Class Passed Successfully!");
 		System.out.println("-Running Tests on PostfixCalculator Class...");
 		PostfixCalculatorTest.main(null);
