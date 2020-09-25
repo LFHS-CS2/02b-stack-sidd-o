3e1c09a804c97f0c3486804658f80eb74cb932d0
sidd-o <69946996+sidd-o@users.noreply.github.com>
2020-09-24 22:12:47 -0500

    Update ArrayStack.java

--- ArrayStack.java
+++ ArrayStack.java
@@ -1,61 +1,82 @@
 /**
- * ArrayStack should implement the Stack interface.
- * You should write your own test cases in ArrayStackTest
+ * ArrayStack implements the Stack interface using Arrays
  *
- * @author (your name)
+ * @author Sidd O.
  */
-public class ArrayStack implements Stack
-{
+public class ArrayStack implements Stack {
+	// Only Storage Used by the Class
 	Object[] temp;
-	public ArrayStack(){
+
+	/**
+	 * Initializes an ArrayStack With No Objects (isEmpty)
+	 */
+	public ArrayStack() {
 		temp = new Object[0];
 	}
 
-	public void push(Object obj){
+	/*
+	 * Add the given value to the top of the stack.
+	 */
+	public void push(Object obj) {
 		Object[] temp3 = new Object[temp.length + 1];
-		for(int i = 0; i < temp.length; i++){
+		for (int i = 0; i < temp.length; i++) {
 			temp3[i] = temp[i];
 		}
 		temp3[temp3.length - 1] = obj;
 		temp = temp3;
 	}
 
-	public Object pop(){
-		try{
-		if(temp.length == 0){
-			throw new StackException("Stack Is Already Empty...");
-		}
-		Object temp1 = new Object();
-		temp1 = temp[temp.length - 1];
-		Object[] temp0 = new Object[temp.length - 1];
-		for(int i = 0; i < temp0.length; i++){
-			temp0[i] = temp[i];
-		}
-		temp = temp0;
+	/**
+	 * Remove and return the value that has been in the ArrayStack the least time.
+	 * Throws a StackException if ArrayStack is Already Empty
+	 */
+	public Object pop() {
+		try {
+			if (temp.length == 0) {
+				throw new StackException("Stack Is Already Empty...");
+			}
+			Object temp1 = new Object();
+			temp1 = temp[temp.length - 1];
+			Object[] temp0 = new Object[temp.length - 1];
+			for (int i = 0; i < temp0.length; i++) {
+				temp0[i] = temp[i];
+			}
+			temp = temp0;
 
-		return temp1;
+			return temp1;
 
-		} catch(StackException e){
+		} catch (StackException e) {
 			System.out.println(e);
 		}
 		return null;
-		
+
 	}
 
-	public Object peekTop(){
-		try{
-		if(temp.length == 0){
-			throw new StackException("Stack Is Already Empty...");
-		}
-		return temp[temp.length - 1];
+	/**
+	 * Return the value that pop would give, without modifying the ArrayStack.
+	 * Throws a StackException if the ArrayStack is empty.
+	 */
+	public Object peekTop() {
+		try {
+			if (temp.length == 0) {
+				throw new StackException("Stack Is Already Empty...");
+			}
+			return temp[temp.length - 1];
 
-		}catch(StackException e){
+		} catch (StackException e) {
 			System.out.println(e);
 		}
 		return null;
 	}
 
-	public boolean isEmpty(){
-		return(temp.length == 0);
+	/**
+	 *
+	 * Returns Whether the ArrayStack is Empty
+	 *
+	 */
+
+	public boolean isEmpty() {
+		return (temp.length == 0);
 	}
 }
+
