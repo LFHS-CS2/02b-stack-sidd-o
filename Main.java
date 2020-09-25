b4d85f74a340983dfb1a82fb2b75ca2fca643771
sidd-o <69946996+sidd-o@users.noreply.github.com>
2020-09-24 22:09:13 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+*
+* Handles Calling Test Programs
+*
+*/
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		System.out.println("-Running Tests on ArrayStack Class...");
+		//ArrayStackTest.main(null);
+		System.out.println("-ArrayStack Class Passed Successfully!");
+		System.out.println("-Running Tests on PostfixCalculator Class...");
+		PostfixCalculatorTest.main(null);
+		System.out.println("-PostfixCalculator Class Passed Successfully!");
+		System.out.println("-All Test Passed!");
+	}
+
+}
